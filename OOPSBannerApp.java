e5ce7fe2b12812de73a914cf0c06f07ba492d4b1
Joshitha CH <joshithachpersonal17@gmail.com>
2026-02-17 08:52:56 +0530

    UC2: Print OOPS banner using stars

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,17 @@
 public class OOPSBannerApp {
-        
-		public static void main(String[] args) {
-		    System.out.println("OOPS");
-			
-		 }
-	}
-	
+    public static void main(String[] args) {
+
+        System.out.println("UC1: Print OOPS to console");
+        System.out.println("OOPS");
+
+        System.out.println("\nUC2: Print OOPS Banner\n");
+
+        System.out.println("  *****    *****    *****    *****  ");
+        System.out.println(" *     *  *     *  *     *  *     * ");
+        System.out.println(" *     *  *     *  *        *       ");
+        System.out.println(" *     *  *     *  *  ***   *  ***  ");
+        System.out.println(" *     *  *     *  *     *  *     * ");
+        System.out.println(" *     *  *     *  *     *  *     * ");
+        System.out.println("  *****    *****    *****    *****  ");
+    }
+}
